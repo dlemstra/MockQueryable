cba9582eb814d6a89161c96284e82fed9d9e58a0
Dirk Lemstra <dirk@lemstra.org>
2020-04-23 09:32:00 +0200

    Make a separate class for the QueryProvider

--- src/MockQueryable/MockQueryable/TestAsyncEnumerable.cs
+++ src/MockQueryable/MockQueryable/TestAsyncEnumerable.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
@@ -9,92 +7,36 @@ using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace MockQueryable
 {
-	public class TestAsyncEnumerable<T> : IAsyncEnumerable<T>, IOrderedQueryable<T>, IAsyncQueryProvider
+	public class TestAsyncEnumerable<T> : TestQueryProvider<T>, IAsyncEnumerable<T>, IAsyncQueryProvider
 	{
-		private IEnumerable<T> _enumerable;
-
 		public TestAsyncEnumerable(Expression expression)
+			: base(expression)
 		{
-			Expression = expression;
 		}
 
 		public TestAsyncEnumerable(IEnumerable<T> enumerable)
+			: base(enumerable)
 		{
-			_enumerable = enumerable;
 		}
+
+		protected override IQueryable<TEntity> CreateInstanceFromExpression<TEntity>(Expression expression)
+			=> new TestAsyncEnumerable<TEntity>(expression);
 
 		public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
-		{
-			return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
-		}
-
-		public IQueryable CreateQuery(Expression expression)
-		{
-			if (expression is MethodCallExpression m)
-			{
-				var resultType = m.Method.ReturnType; // it should be IQueryable<T>
-				var tElement = resultType.GetGenericArguments().First();
-				var queryType = typeof(TestAsyncEnumerable<>).MakeGenericType(tElement);
-				return (IQueryable) Activator.CreateInstance(queryType, expression);
-			}
-
-			return new TestAsyncEnumerable<T>(expression);
-		}
-
-		public IQueryable<TEntity> CreateQuery<TEntity>(Expression expression)
-		{
-			return new TestAsyncEnumerable<TEntity>(expression);
-		}
-
-		public object Execute(Expression expression)
-		{
-			return CompileExpressionItem<object>(expression);
-		}
-
-		public TResult Execute<TResult>(Expression expression)
-		{
-			return CompileExpressionItem<TResult>(expression);
-		}
+			=> new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
 
 		public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
 		{
 			var expectedResultType = typeof(TResult).GetGenericArguments()[0];
 			var executionResult = typeof(IQueryProvider)
-        .GetMethods()
-        .First(method => method.Name == nameof(IQueryProvider.Execute) && method.IsGenericMethod)
-        .MakeGenericMethod(expectedResultType)
+				.GetMethods()
+				.First(method => method.Name == nameof(IQueryProvider.Execute) && method.IsGenericMethod)
+				.MakeGenericMethod(expectedResultType)
 				.Invoke(this, new[] {expression});
 
 			return (TResult) typeof(Task).GetMethod(nameof(Task.FromResult))
 				.MakeGenericMethod(expectedResultType)
 				.Invoke(null, new[] {executionResult});
-		}
-
-
-		IEnumerator<T> IEnumerable<T>.GetEnumerator()
-		{
-			if (_enumerable == null) _enumerable = CompileExpressionItem<IEnumerable<T>>(Expression);
-			return _enumerable.GetEnumerator();
-		}
-
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			if (_enumerable == null) _enumerable = CompileExpressionItem<IEnumerable<T>>(Expression);
-			return _enumerable.GetEnumerator();
-		}
-
-		public Type ElementType => typeof(T);
-
-		public Expression Expression { get; }
-
-		public IQueryProvider Provider => this;
-
-		private static TResult CompileExpressionItem<TResult>(Expression expression)
-		{
-			var rewriter = new TestExpressionVisitor();
-			var body = rewriter.Visit(expression);
-			var f = Expression.Lambda<Func<TResult>>(body, (IEnumerable<ParameterExpression>) null);
-			return f.Compile()();
 		}
 	}
 }
---
+++ src/MockQueryable/MockQueryable/TestQueryProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MockQueryable
+{
+	public abstract class TestQueryProvider<T> : IOrderedQueryable<T>, IQueryProvider
+	{
+		private IEnumerable<T> _enumerable;
+
+		public TestQueryProvider(Expression expression)
+		{
+			this.Expression = expression;
+			this._enumerable = CompileExpressionItem<IEnumerable<T>>(this.Expression);
+		}
+
+		public TestQueryProvider(IEnumerable<T> enumerable)
+		{
+			this._enumerable = enumerable;
+			this.Expression = enumerable.AsQueryable().Expression;
+		}
+
+		public Type ElementType => typeof(T);
+
+		public Expression Expression { get; }
+
+		public IQueryProvider Provider => this;
+
+		public IQueryable CreateQuery(Expression expression)
+			=> this.CreateInstanceFromExpression<T>(expression);
+
+		public IQueryable<TEntity> CreateQuery<TEntity>(Expression expression)
+			=> this.CreateInstanceFromExpression<TEntity>(expression);
+
+		public object Execute(Expression expression)
+			=> CompileExpressionItem<object>(expression);
+
+		public TResult Execute<TResult>(Expression expression)
+			=> CompileExpressionItem<TResult>(expression);
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+			=> this._enumerable.GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator()
+			=> this._enumerable.GetEnumerator();
+
+		protected abstract IQueryable<TEntity> CreateInstanceFromExpression<TEntity>(Expression expression);
+
+		private static TResult CompileExpressionItem<TResult>(Expression expression)
+		{
+			var rewriter = new TestExpressionVisitor();
+			var body = rewriter.Visit(expression);
+			var f = Expression.Lambda<Func<TResult>>(body, (IEnumerable<ParameterExpression>)null);
+			return f.Compile()();
+		}
+	}
+}
